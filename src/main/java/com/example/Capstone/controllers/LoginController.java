07db2c6a9a03eb2191e95ac8563ad922cb4473dc
David Segura <davidsegura838@gmail.com>
2021-04-08 15:32:16 -0500

    removed mapping in login controller

--- src/main/java/com/example/Capstone/controllers/LoginController.java
+++ src/main/java/com/example/Capstone/controllers/LoginController.java
@@ -62,9 +62,9 @@ public class LoginController {
 	     return modelAndView;
 	 }
 	 
-	  @GetMapping(value = "/user/logout")
-	  public String logout() {
-		  
-		  return "login.html";
-	  }
+//	  @GetMapping(value = "/user/logout")
+//	  public String logout() {
+//		  
+//		  return "login.html";
+//	  }
 }
