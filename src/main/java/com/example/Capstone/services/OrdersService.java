a0434c7544735f56e3571842e6fff09530029f6f
David Segura <davidsegura838@gmail.com>
2021-04-09 10:39:10 -0500

    added order repository, services, and orders table in account.html

---
+++ src/main/java/com/example/Capstone/services/OrdersService.java
@@ -0,0 +1,38 @@
+package com.example.Capstone.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Capstone.entities.Orders;
+import com.example.Capstone.entities.User;
+import com.example.Capstone.repositories.OrdersRepository;
+
+@Service
+public class OrdersService {
+	
+	@Autowired
+	OrdersRepository ordersRepository;
+	
+	@Autowired
+	UserService userService;
+	
+	public Iterable<Orders> GetOrdersByUser(User user){
+		return(ordersRepository.findAllByUser(user));
+	}
+	
+	public Orders getOrdersById(int id) {
+		return(ordersRepository.findById(id));
+	}
+	
+	public Orders addOrder(Orders order) {
+		return ordersRepository.save(order);
+	}
+	
+	public Orders updateOrder(Orders orderToUpdate) {
+		return ordersRepository.save(orderToUpdate);
+	}
+	
+	public void deleteOrder(int id) {
+		ordersRepository.deleteById(id);
+	}
+}
