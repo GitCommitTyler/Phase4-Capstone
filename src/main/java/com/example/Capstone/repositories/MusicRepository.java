e72d56dbf4a605e231db50a23ee9f860802698f4
jonathan <jonathanjimenez0745@gmail.com>
2021-04-11 21:05:16 -0400

    commit to see if i can get help on issue

--- src/main/java/com/example/Capstone/repositories/MusicRepository.java
+++ src/main/java/com/example/Capstone/repositories/MusicRepository.java
@@ -2,9 +2,13 @@ package com.example.Capstone.repositories;
 
 import com.example.Capstone.entities.Album;
 import com.example.Capstone.entities.Music;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface MusicRepository extends CrudRepository<Music, Integer>{
 	
 	Iterable<Music> findByAlbum(Album album);
+	Optional<Music> findById(Long id);
 }
